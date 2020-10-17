7f3e68a3fc1518080f466002a54b37c03097d8ab
Kyryl Step <skiril2000@ukr.net>
2020-10-17 08:06:01 +0300

    Add project files.

---
+++ C_Sharp_App1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C_Sharp_App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string name = "Bob";
+            int mail = 3;
+            decimal temeprature = 34.4m;
+            Console.WriteLine("Hello, {0}! You have {1} in inbox.  Temperaure id {2} celsius", name, mail, temeprature);
+            string s = $"Hello, {name}! You have {mail} in inbox.  Temperaure id {temeprature} celsius";
+            Console.WriteLine(s);
+
+        }
+    }
+}
